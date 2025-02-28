b24564b4957f79558af659d283c0b93a1415e500
britojp <joaopedrodebritotome@discente.ufg.br>
2025-02-28 16:19:41 -0300

    fix : Correção carousel de filmes populares

--- src/components/index/popularFilms.vue
+++ src/components/index/popularFilms.vue
@@ -1,93 +1,108 @@
 <template>
-  <template
-    v-for="film in popularFilms"
-    :key="film.id"
-  >
-  <v-hover v-slot="{ isHovering, props }">
-      <v-card
+  <div class="d-flex flex-row mb-6">
+    <template v-if="popularFilms.length">
+      <v-carousel
+        v-model="currentSlide"
+        cycle
+        hide-delimiters
+        :show-arrows="true"
         class="mx-auto"
-        max-width="344"
-        v-bind="props"
       >
-      <v-img :src="film.poster_path ? `https://image.tmdb.org/t/p/w500${film.backdrop_path}` : 'https://cdn.vuetifyjs.com/images/cards/forest-art.jpg'" />
-
-
-        <v-card-text>
-          <h2 class="text-h6 text-primary">
-            {{ film.title }}
-          </h2>
-          {{ film.overview }}
-        </v-card-text>
-          
-        <v-card-title>
-          <v-rating
-            :model-value=" calculateRate(film)"
-            class="me-2"
-            color="orange"
-            density="compact"
-            hover
-          />
-          <span class="text-primary text-subtitle-2">Popularidade: {{ film.popularity.toFixed(0) }}</span>
-        </v-card-title>
-          
-        <v-overlay
-          :model-value="!!isHovering"
-          class="align-center justify-center"
-          scrim="#036358"
-          contained
+        <v-carousel-item
+          v-for="(group, index) in groupedFilms"
+          :key="index"
         >
-          <v-btn 
-            variant="flat"
-          >
-            See more info
-          </v-btn>
-        </v-overlay>
-      </v-card>
-    </v-hover>
-  </template>
+          <v-row class="d-flex justify-center">
+            <v-col
+              v-for="film in group"
+              :key="film.id"
+              cols="12"
+              md="4"
+              sm="6"
+            >
+              <v-hover v-slot="{ props }">
+                <v-card
+                  max-width="344"
+                  v-bind="props"
+                  class="ma-2 pa-2 mx-auto"
+                >
+                  <v-img
+                    :src="film.poster_path ? `https://image.tmdb.org/t/p/w500${film.backdrop_path}` : 'https://cdn.vuetifyjs.com/images/cards/forest-art.jpg'"
+                  />
+                  <v-card-text>
+                    <h2 class="text-h6 text-primary">{{ film.title }}</h2>
+                    {{ film.overview }}
+                  </v-card-text>
+                  <v-card-title>
+                    <v-rating
+                      :model-value="calculateRate(film)"
+                      class="me-2"
+                      color="orange"
+                      density="compact"
+                      hover
+                    />
+                    <span class="text-primary text-subtitle-2">Popularidade: {{ film.popularity.toFixed(0) }}</span>
+                  </v-card-title>
+                  <v-overlay
+                    class="align-center justify-center"
+                    scrim="#036358"
+                    contained
+                  >
+                    <v-btn variant="flat">See more info</v-btn>
+                  </v-overlay>
+                </v-card>
+              </v-hover>
+            </v-col>
+          </v-row>
+        </v-carousel-item>
+      </v-carousel>
+    </template>
+  </div>
 </template>
 
 <script lang="ts">
-import { getMostPopularMovies } from '@/services/api'; 
-import { Film } from '../components/types/types'
+import { getMostPopularMovies } from '@/services/api';
+import { Film } from '../components/types/types';
 
-export default{
-
+export default {
   name: 'PopularFilms',
-  data(){
-    return{
-    popularFilms : [] as Film[],
-    
+  data() {
+    return {
+      popularFilms: [] as Film[],
+      currentSlide: 0,
     };
   },
-
-  methods :  {
-    async loadPopularMovies (){
+  computed: {
+    groupedFilms() {
+      const groups = [];
+      for (let i = 0; i < this.popularFilms.length; i += 3) {
+        groups.push(this.popularFilms.slice(i, i + 3));
+      }
+      return groups;
+    },
+  },
+  mounted() {
+    this.loadPopularMovies();
+  },
+  methods: {
+    async loadPopularMovies() {
       try {
-        const data = await getMostPopularMovies(1); 
-        this.popularFilms = data.results; 
+        const data = await getMostPopularMovies(1);
+        this.popularFilms = data.results;
         console.log(this.popularFilms);
       } catch (error) {
         console.error('Erro ao carregar filmes populares:', error);
       }
     },
     calculateRate(film: Film) {
-  if (film.vote_average <= 2) return 1;
-  if (film.vote_average <= 4) return 2;
-  if (film.vote_average <= 6) return 3;
-  if (film.vote_average <= 7) return 4;
-  return 5;
-}
-
-
-},
-
-  mounted(){
-    this.loadPopularMovies();
+      if (film.vote_average <= 2) return 1;
+      if (film.vote_average <= 4) return 2;
+      if (film.vote_average <= 6) return 3;
+      if (film.vote_average <= 7) return 4;
+      return 5;
+    },
   },
-  
-}
-
+};
 </script>
 
 <style lang="css">
