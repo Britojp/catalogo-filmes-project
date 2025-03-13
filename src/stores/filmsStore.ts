fd36247e9501097c66f11324dc4ff2765048e216
britojp <joaopedrodebritotome@discente.ufg.br>
2025-03-13 11:59:51 -0300

    fix: Correção da sobrecarga de páginas salvas

--- src/stores/filmsStore.ts
+++ src/stores/filmsStore.ts
@@ -1,7 +1,6 @@
 import { defineStore } from 'pinia';
 import type Film from '@/types/types';
-import { searchMovies } from '@/services/api';
-import debounce from 'lodash/debounce';
+
 
 export const useFilmsStore = defineStore('filmsStore', {
   state() {
@@ -13,25 +12,18 @@ export const useFilmsStore = defineStore('filmsStore', {
   },
   actions: {
 
-  handleSearchQuery: debounce(function(this : any) {
-    if (this.searchQueryText && this.searchQueryText.length > 3) { 
-      searchMovies(this.searchQueryText)
-        .then((response) => {
-          this.searchMovies = response.data.results || []; 
-        })
-        .catch((error) => {
-          console.error('Erro ao buscar filmes:', error);
-        })
-        .finally(() => {
-        });
-    } else {
-      this.searchMovies = [];
-    }
-  }, 500),
-
   addMoviesForPage(page: number, movies: Film[]) {
     this.allMovies[page] = movies;
+    this.removeFilms();
   },
+
+  removeFilms() {
+    if (Object.keys(this.allMovies).length > 6) {
+      const firstPage = Math.min(...Object.keys(this.allMovies).map(Number));
+      delete this.allMovies[firstPage];
+    }
+  },
+
 
 },
 
